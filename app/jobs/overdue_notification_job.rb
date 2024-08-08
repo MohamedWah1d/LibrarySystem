9abbe2cc27bde77fa9b217142a0eafee98cdd0fc
MohamedWah1d <123986456+MohamedWah1d@users.noreply.github.com>
2024-08-08 17:47:05 +0300

    [UPDATE] fixed a notification issue

--- app/jobs/overdue_notification_job.rb
+++ app/jobs/overdue_notification_job.rb
@@ -2,11 +2,13 @@ class OverdueNotificationJob < ApplicationJob
   queue_as :default
 
   def perform
-    overdue_requests = BorrowRequest.where("intended_return_date < ? AND return_date IS NULL", Time.current)
-    admins = User.where(admin: true)
-
+    overdue_requests = BorrowRequest.where('intended_return_date < ? AND return_date IS NULL', Time.current)
     overdue_requests.each do |request|
       NotificationMailer.overdue_book_notification(request.user, request.book).deliver_now
+    end
+
+    admins = User.where(user_type: :admin)
+    overdue_requests.each do |request|
       admins.each do |admin|
         NotificationMailer.admin_overdue_notification(admin, request.book).deliver_now
       end
